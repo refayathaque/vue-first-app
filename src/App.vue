3b3f2fb758416feb7fd5c2da3a00950d564c4cbf
Refayat Haque <rhaque@pyramidsystems.com>
2020-10-09 00:40:09 -0400

    Fri Oct 9 0:40

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <h2>My friends</h2>
   <ul>
-    <friend-contact v-for="(friend, index) in friends" :key="index" :name="friend.name" :phone-number="friend.phoneNumber" :email-address="friend.emailAddress" :is-favorite="friend.isFavorite"></friend-contact>
+    <li v-for="friend in friends" :key="friend.id">{{ friend.name }}</li>  
+  </ul>
+    <friend-contact v-for="friend in friends" :key="friend.id" :name="friend.name" :phone-number="friend.phoneNumber" :email-address="friend.emailAddress" :is-favorite="friend.isFavorite" :is-intelligent="friend.isIntelligent" @toggle-intelligence="toggleIntelligenceChange"></friend-contact>
+
+    <!-- toggle intelligence won't work for these -->
     <friend-contact name="John Lorenz" phone-number="2023415049" :is-favorite="1"></friend-contact>
     <friend-contact name="Jorge Lorenz" phone-number="2023415044" email-address="jo.lorenz@gmail.com" :is-favorite="0"></friend-contact>
     <friend-contact name="Tilda Lorenz" email-address="t.lorenz@gmail.com" :is-favorite="1"></friend-contact>
-    <li v-for="(friend, index) in friends" :key="index">{{ friend.name }}</li>
-  </ul>
 </template>
 
 <script>
@@ -15,20 +17,36 @@ export default {
     return {
       friends: [
         {
-          id: 'Manuel',
+          id: 1,
           name: 'Manuel Lorenz',
           phoneNumber: '2023415040',
           emailAddress: 'm.lorenz@gmail.com',
-          isFavorite: 1
+          isFavorite: 1,
+          isIntelligent: true
         },
         {
-          id: 'Julie',
+          id: 2,
           name: 'Julie Lorenz',
-          phoneNumber: '2023415041 ',
+          phoneNumber: '2023415041',
           emailAddress: 'j.lorenz@gmail.com',
-          isFavorite: 0
+          isFavorite: 0,
+          isIntelligent: true
+        },
+        {
+          id: 3,
+          name: 'Timothy Lorenz',
+          phoneNumber: '2023415099',
+          emailAddress: 'timo.lorenz@gmail.com',
+          isFavorite: 0,
+          isIntelligent: true
         }
       ]
+    }
+  },
+  methods: {
+    toggleIntelligenceChange(emailAddress) {
+      const identifiedFriend = this.friends.find(friend => friend.emailAddress === emailAddress )
+      identifiedFriend.isIntelligent = !identifiedFriend.isIntelligent
     }
   }
 }
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,12 +1,12 @@
 <template>
-  <li>
-    <h2>{{ name }} {{ isFavorite === 1 ? '(Favorite)' : '' }}</h2>
-    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
-    <ul v-if="detailsAreVisible">
-      <li>Phone: {{ phoneNumber }}</li>
-      <li>Email: {{ emailAddress }}</li>
-    </ul>
-  </li>
+  <h3>{{ name }} {{ isFavorite === 1 ? '(Favorite)' : '' }}</h3>
+  <h4>{{ isIntelligent ? 'Intelligent' : '' }}</h4>
+  <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+  <button @click="toggleIntelligence">Make {{ isIntelligent ? 'un' : '' }}intelligent</button>
+  <ul v-if="detailsAreVisible">
+    <li>Phone: {{ phoneNumber }}</li>
+    <li>Email: {{ emailAddress }}</li>
+  </ul>
 </template>
 
 <script>
@@ -31,24 +31,43 @@ export default {
       required: true,
       validator: (value) => {
         return value === 0 || value === 1
+        // 0 is false, 1 is true
       }
+    },
+    isIntelligent: {
+      type: Boolean,
+      required: false,
+      default: false
     }
+  },
+  // emits: [ 'toggle-favorite' ],
+  emits: { 
+    'toggle-intelligence': (emailAddress) => {
+      if (emailAddress) {
+        return true
+      } else {
+        console.warn ('emailAddress is missing!')
+        return false
+      }
+    }   
   },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: 'Manuel',
-        name: 'Manuel Lorenz',
-        phoneNumber: '2023415040',
-        emailAddress: 'm.lorenz@gmail.com'
-      }
+      // friend: {
+      //   id: 'Manuel',
+      //   name: 'Manuel Lorenz',
+      //   phoneNumber: '2023415040',
+      //   emailAddress: 'm.lorenz@gmail.com'
+      // }
     }
   },
   methods: {
     toggleDetails() {
-      console.log(this.name, this.phoneNumber, this.emailAddress)
       this.detailsAreVisible = !this.detailsAreVisible
+    },
+    toggleIntelligence() {
+      this.$emit('toggle-intelligence', this.emailAddress);
     }
   }
 }
