ee0f5a478dc23d93780650c4f75cfb2c972cbe6b
Refayat Haque <rhaque@pyramidsystems.com>
2020-10-10 02:32:39 -0400

    Sat Oct 10 2:32

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone Number</label>
+      <input type="tel" v-model="enteredPhoneNumber" />
+    </div>
+    <div>
+      <label>Email Address</label>
+      <input type="email" v-model="enteredEmailAddress" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: [ 'add-contact' ],
+  data() {
+    return {
+      enteredName: '',
+      enteredPhoneNumber: '',
+      enteredEmailAddress: ''
+    }
+  },
+  methods: {
+    submitData() {
+      this.$emit('add-contact', this.enteredName, this.enteredPhoneNumber, this.enteredEmailAddress)
+    }
+  }
+}
+</script>
